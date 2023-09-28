eb326b8ef66d0095c9139ea21c6aef93d3008285
Tom Ebergen <tom@ebergen.com>
2023-09-28 11:57:36 +0200

    change update scripts to use slightly different versions of duckdb

---
+++ duckdb-latest/upg-duckdb-latest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# upgrade all packages in duckdb library only if new arrow is out
+echo 'upgrading duckdb-latest, installing 0.9.1'
+
+rm -rf ./duckdb-latest/r-duckdb-latest
+mkdir -p ./duckdb-latest/r-duckdb-latest
+
+
+cd duckdb-latest
+git clone https://github.com/duckdb/duckdb-r
+cd duckdb-r 
+git checkout v0.9.0
+cd ..
+ncores=$(nproc --all)
+MAKE="make -j$ncores" R CMD INSTALL -l "./r-duckdb-latest" duckdb-r
+rm -rf duckdb-r
+cd ..
--- duckdb/upg-duckdb.sh
+++ duckdb/upg-duckdb.sh
@@ -2,5 +2,21 @@
 set -e
 
 # upgrade all packages in duckdb library only if new arrow is out
-echo 'upgrading duckdb...'
-Rscript -e 'ap=available.packages(repos="https://cloud.r-project.org/"); if (ap["duckdb","Version"]!=packageVersion("duckdb", lib.loc="./duckdb/r-duckdb")) update.packages(lib.loc="./duckdb/r-duckdb", ask=FALSE, checkBuilt=TRUE, quiet=TRUE, repos="https://cloud.r-project.org/")'
+echo 'upgrading duckdb, installing 0.8.1'
+
+rm -rf ./duckdb/r-duckdb
+mkdir -p ./duckdb/r-duckdb
+
+
+cd duckdb
+git clone https://github.com/duckdb/duckdb-r
+cd duckdb-r 
+git checkout v0.8.1
+cd ..
+ncores=$(nproc --all)
+MAKE="make -j$ncores" R CMD INSTALL -l "./r-duckdb" duckdb-r
+rm -rf duckdb-r
+cd ..
+
+
+# Rscript -e 'ap=available.packages(repos="https://cloud.r-project.org/"); if (ap["duckdb","Version"]!=packageVersion("duckdb", lib.loc="./duckdb/r-duckdb")) update.packages(lib.loc="./duckdb/r-duckdb", ask=FALSE, checkBuilt=TRUE, quiet=TRUE, repos="https://cloud.r-project.org/")'
