e54b17fd80fc6bc57be214bb88e5c849fa5b515a
Tom Ebergen <Tmonster@users.noreply.github.com>
2024-07-11 10:10:42 +0200

    New polars Times v1.1.0 (#87)
    New polars times for v1.1.0

--- polars/groupby-polars.py
+++ polars/groupby-polars.py
@@ -24,7 +24,7 @@ src_grp = os.path.join("data", data_name + ".csv")
 print("loading dataset %s" % data_name, flush=True)
 
 with pl.StringCache():
-    x = (pl.read_csv(src_grp, schema_overrides={"id4":pl.Int32, "id5":pl.Int32, "id6":pl.Int32, "v1":pl.Int32, "v2":pl.Int32, "v3":pl.Float32}, low_memory=True, rechunk=True)
+    x = (pl.read_csv(src_grp, schema_overrides={"id4":pl.Int32, "id5":pl.Int32, "id6":pl.Int32, "v1":pl.Int32, "v2":pl.Int32, "v3":pl.Float64}, low_memory=True, rechunk=True)
          .with_columns(pl.col(["id1", "id2", "id3"]).cast(pl.Categorical)))
 
 in_rows = x.shape[0]
