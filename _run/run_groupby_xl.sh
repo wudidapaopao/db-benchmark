28754dbfacbef330526d32982739a62003772315
Tmonster <tom@ebergen.com>
2024-09-05 13:18:38 +0200

    change ~duckdb to duckdb

--- _run/run_groupby_xl.sh
+++ _run/run_groupby_xl.sh
@@ -3,7 +3,7 @@ wget https://duckdb-blobs.s3.amazonaws.com/data/db-benchmark-data/groupby-500gb.
 
 
 # expand groupby-small datasets to csv
-~/duckdb -c groupby-500gb.duckdb "copy G1_1e10_1e4_10_0 to 'G1_1e10_1e4_10_0.csv' (FORMAT CSV)"
+duckdb -c groupby-500gb.duckdb "copy G1_1e10_1e4_10_0 to 'G1_1e10_1e4_10_0.csv' (FORMAT CSV)"
 
 
 cp ../_control/data_groupby_xlarge.csv ../_control/data.csv
@@ -16,3 +16,6 @@ echo "done..."
 echo "removing data files"
 rm data/*.csv
 rm data/*.duckdb
+
+
+/bin:/usr/bin
--- _run/run_join_xl.sh
+++ _run/run_join_xl.sh
@@ -3,10 +3,10 @@ wget https://duckdb-blobs.s3.amazonaws.com/data/db-benchmark-data/join-500gb.duc
 
 
 # expand groupby-small datasets to csv
-~/duckdb -c join-500gb.duckdb "copy x to 'J1_NA_0_0.csv' (FORMAT CSV)"
-~/duckdb -c join-500gb.duckdb "copy big to 'J1_1e10_0_0.csv' (FORMAT CSV)"
-~/duckdb -c join-500gb.duckdb "copy medim to 'J1_1e7_0_0.csv' (FORMAT CSV)"
-~/duckdb -c join-500gb.duckdb "copy small to 'J1_1e4_0_0.csv' (FORMAT CSV)"
+duckdb -c join-500gb.duckdb "copy x to 'J1_NA_0_0.csv' (FORMAT CSV)"
+duckdb -c join-500gb.duckdb "copy big to 'J1_1e10_0_0.csv' (FORMAT CSV)"
+duckdb -c join-500gb.duckdb "copy medim to 'J1_1e7_0_0.csv' (FORMAT CSV)"
+duckdb -c join-500gb.duckdb "copy small to 'J1_1e4_0_0.csv' (FORMAT CSV)"
 
 
 cp ../_control/data_join_xlarge.csv ../_control/data.csv
