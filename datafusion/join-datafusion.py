1dce3c658a7717a1bb856ccc6e0a98b2ea871c8c
Tmonster <tom@ebergen.com>
2024-09-06 17:05:12 +0200

    fix datafusion

--- datafusion/join-datafusion.py
+++ datafusion/join-datafusion.py
@@ -39,7 +39,7 @@ if len(src_jn_y) != 3:
   raise Exception("Something went wrong in preparing files used for join")
 
 print("loading datasets " + data_name + ", " + y_data_name[0] + ", " + y_data_name[2] + ", " + y_data_name[2], flush=True)
-scale_factor = data_name.replace("G1_","")[:4].replace("_", "")
+scale_factor = data_name.replace("J1_","")[:4].replace("_", "")
 on_disk = 'TRUE' if float(scale_factor) >= 1e10 else 'FALSE'
 
 
