6a4c1bc6c687d114a7acf329268207e5c69339ce
Tmonster <tom@ebergen.com>
2024-09-06 16:04:31 +0200

    fix run.conf and run.sh

--- run.sh
+++ run.sh
@@ -29,6 +29,13 @@ else
   Rscript -e 'swap_all<-data.table::fread("free -h | grep Swap", header=FALSE)[, -1L][, as.numeric(gsub("[^0-9.]", "", unlist(.SD)))]; swap_off<-!is.na(s<-sum(swap_all)) && s==0; q("no", status=as.numeric(swap_off))' && echo "# Benchmark run $BATCH aborted. swap is enabled, 'free -h' has to report only 0s for Swap, run 'swapoff -a' before calling 'run.sh'" && exit;
 fi
 
+
+if [[ $MACHINE_TYPE != 'small' && $MACHINE_TYPE != 'medium' && $MACHINE_TYPE != 'large' ]]; then
+  echo "Machine type is not valid. Must be \`small\` \`medium\` or \`large\` "
+  exit 1
+fi
+
+
 # ensure directories exists
 mkdir -p ./out
 if [[ ! -d ./data ]]; then echo "# Benchmark run $BATCH aborted. './data' directory does not exists" && exit; fi;
