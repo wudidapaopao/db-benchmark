5b3132125e6683a6bab9fee6d709af968f809ea6
Tmonster <tom@ebergen.com>
2024-09-06 16:21:07 +0200

    fix some datafusion things

--- datafusion/join-datafusion.py
+++ datafusion/join-datafusion.py
@@ -39,7 +39,7 @@ if len(src_jn_y) != 3:
   raise Exception("Something went wrong in preparing files used for join")
 
 print("loading datasets " + data_name + ", " + y_data_name[0] + ", " + y_data_name[2] + ", " + y_data_name[2], flush=True)
-on_disk = 'TRUE' if float(SRC_DATANAME.replace("J1_", "")[:4]) >= 1e10 else 'FALSE'
+on_disk = 'TRUE' if float(data_name.replace("J1_", "")[:4]) >= 1e10 else 'FALSE'
 
 
 ctx = df.SessionContext()
