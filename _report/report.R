491e61ac03c96f6ed4df7b40ccd7729f8a95cd6a
Tmonster <tom@ebergen.com>
2025-01-15 09:22:51 +0100

    have scripts handle new machine type column

--- _report/report.R
+++ _report/report.R
@@ -24,6 +24,11 @@ get_data_levels = function() {
 
   list(groupby=groupby, join=join, groupby2014=groupby2014)
 }
+
+get_machine_types = function() {
+  c('xlarge', 'small')
+}
+
 get_excluded_batch = function() {
   c(
     1552478772L, 1552482879L # testing different data as 1e9_1e2_0_0 to test logical compression of measures
@@ -123,7 +128,7 @@ model_time = function(d) {
   }
   # d[solution=="polars" & data%like%"G1_1e[7|8]_1e2_5_0" & run==1L & {z=tail(unique(batch, na.rm=TRUE), 3); print(z); batch%in%z}][, dcast(.SD, data+question~batch+version, value.var="chk")]
   if (nrow(
-    d[!is.na(chk), .(unqn1_chk=approxUniqueN1(chk)), .(task, solution, data, question)][unqn1_chk==FALSE]
+    d[!is.na(chk), .(unqn1_chk=approxUniqueN1(chk)), .(task, solution, data, question, machine_size)][unqn1_chk==FALSE]
     )) stop("Value of 'chk' varies for different runs for single solution+question")
   #d[,.SD][!is.na(chk), `:=`(unq_chk=approxUniqueN1(chk), paste_unq_chk=paste(unique(chk), collapse=",")), .(task, data, question)][unq_chk==FALSE, .(paste_unq_chk), .(task, solution, data, question)]
   #d[solution=="polars" & data%like%"G1_1e[7|8]_1e2_5_0" & run==1L & {z=tail(unique(batch, na.rm=TRUE), 3); print(z); batch%in%z}][, dcast(.SD, data+question~batch+version, value.var="out_rows")]
