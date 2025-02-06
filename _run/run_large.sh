ea86c8b6c2eb6fef10a03af484d000ae0eac0761
Tmonster <tom@ebergen.com>
2025-02-06 08:55:09 +0100

    always try to run large join even on small machine

--- _run/run_large.sh
+++ _run/run_large.sh
@@ -6,7 +6,7 @@ aws s3 cp s3://duckdb-blobs/data/db-benchmark-data/groupby_large.duckdb data/gro
 aws s3 cp s3://duckdb-blobs/data/db-benchmark-data/join_large.duckdb data/join_large.duckdb --no-sign-request
 
 
-# expand groupby-small datasets to csv
+# expand groupby-large datasets to csv
 duckdb data/groupby_large.duckdb  -c "copy G1_1e9_1e2_0_0 to 'data/G1_1e9_1e2_0_0.csv' (FORMAT CSV)"
 duckdb data/groupby_large.duckdb  -c "copy G1_1e9_1e1_0_0 to 'data/G1_1e9_1e1_0_0.csv' (FORMAT CSV)"
 duckdb data/groupby_large.duckdb  -c "copy G1_1e9_2e0_0_0 to 'data/G1_1e9_2e0_0_0.csv' (FORMAT CSV)"
@@ -14,23 +14,14 @@ duckdb data/groupby_large.duckdb  -c "copy G1_1e9_1e2_0_1 to 'data/G1_1e9_1e2_0_
 duckdb data/groupby_large.duckdb  -c "copy G1_1e9_1e2_5_0 to 'data/G1_1e9_1e2_5_0.csv' (FORMAT CSV)"
 
 
-if [[ $MACHINE_TYPE != 'c6id.4xlarge' ]]
-then
-	# expand join-small datasets to csv
-	duckdb data/join_large.duckdb  -c "copy J1_1e9_NA_0_0 to 'data/J1_1e9_NA_0_0.csv' (FORMAT CSV)"
-	duckdb data/join_large.duckdb  -c "copy J1_1e9_1e9_0_0 to 'data/J1_1e9_1e9_0_0.csv' (FORMAT CSV)"
-	duckdb data/join_large.duckdb  -c "copy J1_1e9_1e6_0_0 to 'data/J1_1e9_1e6_0_0.csv' (FORMAT CSV)"
-	duckdb data/join_large.duckdb  -c "copy J1_1e9_1e3_0_0 to 'data/J1_1e9_1e3_0_0.csv' (FORMAT CSV)"
-fi
+# expand join-large datasets to csv
+duckdb data/join_large.duckdb  -c "copy J1_1e9_NA_0_0 to 'data/J1_1e9_NA_0_0.csv' (FORMAT CSV)"
+duckdb data/join_large.duckdb  -c "copy J1_1e9_1e9_0_0 to 'data/J1_1e9_1e9_0_0.csv' (FORMAT CSV)"
+duckdb data/join_large.duckdb  -c "copy J1_1e9_1e6_0_0 to 'data/J1_1e9_1e6_0_0.csv' (FORMAT CSV)"
+duckdb data/join_large.duckdb  -c "copy J1_1e9_1e3_0_0 to 'data/J1_1e9_1e3_0_0.csv' (FORMAT CSV)"
 
 
 cp _control/data_large.csv _control/data.csv
-
-# remove 50GB join if machine type is small. No machine can complete it.
-if [[ $MACHINE_TYPE == 'c6id.4xlarge' ]]
-then
-	sed -i -e '/join/d' _control/data.csv
-fi
 
 echo "Running all solutions on large (50GB) datasets"
 ./run.sh
