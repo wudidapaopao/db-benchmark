1a6623d1fff9713cfaa1261985066cc05536f3d4
Tmonster <tom@ebergen.com>
2025-02-10 13:04:05 +0100

    remove references to helpersds

--- juliads/setup-juliads.sh
+++ juliads/setup-juliads.sh
@@ -13,6 +13,6 @@ source path.env
 
 # install julia InMemoryDatasets and csv packages
 julia -q -e 'using Pkg; Pkg.add(["DataFrames", "InMemoryDatasets","DLMReader", "PooledArrays", "Arrow", "CSV"])'
-julia -q -e 'include("$(pwd())/_helpers/helpersds.jl"); pkgmeta = getpkgmeta("InMemoryDatasets"); println(string(pkgmeta["version"])); pkgmeta = getpkgmeta("DLMReader"); println(string(pkgmeta["version"]))'
+julia -q -e 'include("$(pwd())/_helpers/helpers.jl"); pkgmeta = getpkgmeta("InMemoryDatasets"); println(string(pkgmeta["version"])); pkgmeta = getpkgmeta("DLMReader"); println(string(pkgmeta["version"]))'
 
 ./juliadf/ver-juliadf.sh
