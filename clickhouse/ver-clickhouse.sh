6a75d3838abb46d12ff76e53128cfc44fa12fd2d
Tmonster <tom@ebergen.com>
2025-02-26 10:34:49 +0100

    make sure only new upgraded solutions are tested

--- clickhouse/ver-clickhouse.sh
+++ clickhouse/ver-clickhouse.sh
@@ -4,3 +4,6 @@ set -e
 source ./clickhouse/ch.sh # clickhouse helper scripts
 
 ch_installed && clickhouse-client --version-clean > clickhouse/VERSION && echo "" > clickhouse/REVISION
+
+sudo chown $whoami:$whoami clickhouse/VERSION
+sudo chown $whoami:$whoami clickhouse/REVISION
