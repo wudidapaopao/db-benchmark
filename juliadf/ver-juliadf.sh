ea038c7df4ef5fddf818d7f794f4b389ea9b4281
Tmonster <tom@ebergen.com>
2025-02-28 09:13:40 +0100

    fix some julia scripts

--- juliadf/ver-juliadf.sh
+++ juliadf/ver-juliadf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
+source path.env
 
 julia -q -e 'include("$(pwd())/_helpers/helpers.jl"); pkgmeta = getpkgmeta("DataFrames"); f=open("juliadf/VERSION","w"); write(f, string(pkgmeta["version"])); f=open("juliadf/REVISION","w"); write(f, string(pkgmeta["git-tree-sha1"]));' > /dev/null
