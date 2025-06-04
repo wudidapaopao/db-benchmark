1dddae6d7e6c942b132ce679bb45e821386dc46d
Tmonster <tom@ebergen.com>
2025-06-04 14:37:46 +0200

    speed up duckdb builds

--- duckdb/setup-duckdb.sh
+++ duckdb/setup-duckdb.sh
@@ -7,6 +7,7 @@ mkdir -p ./duckdb/r-duckdb
 # Rscript -e  'withr::with_libpaths(new = "./duckdb/r-duckdb", devtools::install_github("duckdb/duckdb/tools/rpkg"))'
 # prevent errors when running 'ver-duckdb.sh'
 Rscript -e 'install.packages("DBI", lib="./duckdb/r-duckdb", repos = "http://cloud.r-project.org")'
-Rscript -e 'install.packages("duckdb", lib="./duckdb/r-duckdb", repos = "http://cloud.r-project.org")'
+ncores=`python3 -c 'import multiprocessing as mp; print(mp.cpu_count())'`
+MAKE="make -j$ncores" Rscript -e 'install.packages("duckdb", lib="./duckdb/r-duckdb", repos = "http://cloud.r-project.org")'
 
 ./duckdb/ver-duckdb.sh
