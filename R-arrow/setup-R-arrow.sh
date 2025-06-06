b1cc30783b657c6c5e394af79f739135120a4f35
Tmonster <tom@duckdblabs.com>
2025-06-06 07:54:41 +0000

    more results

--- R-arrow/setup-R-arrow.sh
+++ R-arrow/setup-R-arrow.sh
@@ -3,6 +3,7 @@ set -e
 
 # install stable arrow
 mkdir -p ./R-arrow/r-arrow
-Rscript -e 'install.packages(c("arrow","dplyr"), lib="./R-arrow/r-arrow")'
+ncores=`python3 -c 'import multiprocessing as mp; print(mp.cpu_count())'`
+MAKE="make -j$ncores" Rscript -e 'install.packages(c("arrow","dplyr"), lib="./R-arrow/r-arrow")'
 
 ./R-arrow/ver-R-arrow.sh
