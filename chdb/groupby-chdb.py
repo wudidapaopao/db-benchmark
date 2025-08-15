69189a21337460088723f73a0d25669ec7884fea
Sergei Giniatulin <kornburn@gmail.com>
2025-08-15 16:34:31 +0300

    Update groupby-chdb.py
    Fixed MergeTree implementation.
    Also added max insert threads settings for better performance

--- chdb/groupby-chdb.py
+++ chdb/groupby-chdb.py
@@ -32,13 +32,17 @@ on_disk = 'TRUE' if (machine_type == "c6id.4xlarge" and float(scale_factor) >= 1
 if on_disk == 'TRUE':
   print("using disk memory-mapped data storage")
   conn = chdb.connect(chdb_join_db)
-  query_engine = 'ENGINE = MergeTree'
+  query_engine = 'ENGINE = MergeTree ORDER BY tuple()'
 else:
   print("using in-memory data storage")
   conn = chdb.connect(":memory:")
-  query_engine = 'ENGINE = Memory'
+  #query_engine = 'ENGINE = Memory'
+  query_engine = 'ENGINE = MergeTree ORDER BY tuple()'
 
 na_flag = int(data_name.split("_")[3])
+
+threads = 6
+settings = f"SETTINGS max_insert_threads={threads}"
 
 engine_type = 'LOG'
 conn.query("CREATE DATABASE IF NOT EXISTS db_benchmark ENGINE = Atomic")
@@ -59,7 +63,7 @@ print("grouping...", flush=True)
 question = "sum v1 by id1" # q1
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, sum(v1) AS v1 FROM db_benchmark.x GROUP BY id1"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, sum(v1) AS v1 FROM db_benchmark.x GROUP BY id1 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -73,7 +77,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, sum(v1) AS v1 FROM db_benchmark.x GROUP BY id1"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, sum(v1) AS v1 FROM db_benchmark.x GROUP BY id1 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -91,7 +95,7 @@ conn.query("DROP TABLE IF EXISTS ans")
 question = "sum v1 by id1:id2" # q2
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, id2, sum(v1) AS v1 FROM db_benchmark.x GROUP BY id1, id2"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, id2, sum(v1) AS v1 FROM db_benchmark.x GROUP BY id1, id2 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -105,7 +109,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, id2, sum(v1) AS v1 FROM db_benchmark.x GROUP BY id1, id2"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, id2, sum(v1) AS v1 FROM db_benchmark.x GROUP BY id1, id2 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -123,7 +127,7 @@ conn.query("DROP TABLE IF EXISTS ans")
 question = "sum v1 mean v3 by id3" # q3
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id3, sum(v1) AS v1, avg(v3) AS v3 FROM db_benchmark.x GROUP BY id3"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id3, sum(v1) AS v1, avg(v3) AS v3 FROM db_benchmark.x GROUP BY id3 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -137,7 +141,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id3, sum(v1) AS v1, avg(v3) AS v3 FROM db_benchmark.x GROUP BY id3"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id3, sum(v1) AS v1, avg(v3) AS v3 FROM db_benchmark.x GROUP BY id3 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -155,7 +159,7 @@ conn.query("DROP TABLE IF EXISTS ans")
 question = "mean v1:v3 by id4" # q4
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id4, avg(v1) AS v1, avg(v2) AS v2, avg(v3) AS v3 FROM db_benchmark.x GROUP BY id4"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id4, avg(v1) AS v1, avg(v2) AS v2, avg(v3) AS v3 FROM db_benchmark.x GROUP BY id4 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -169,7 +173,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id4, avg(v1) AS v1, avg(v2) AS v2, avg(v3) AS v3 FROM db_benchmark.x GROUP BY id4"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id4, avg(v1) AS v1, avg(v2) AS v2, avg(v3) AS v3 FROM db_benchmark.x GROUP BY id4 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -187,7 +191,7 @@ conn.query("DROP TABLE IF EXISTS ans")
 question = "sum v1:v3 by id6" # q5
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id6, sum(v1) AS v1, sum(v2) AS v2, sum(v3) AS v3 FROM db_benchmark.x GROUP BY id6"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id6, sum(v1) AS v1, sum(v2) AS v2, sum(v3) AS v3 FROM db_benchmark.x GROUP BY id6 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -201,7 +205,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id6, sum(v1) AS v1, sum(v2) AS v2, sum(v3) AS v3 FROM db_benchmark.x GROUP BY id6"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id6, sum(v1) AS v1, sum(v2) AS v2, sum(v3) AS v3 FROM db_benchmark.x GROUP BY id6 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -219,7 +223,7 @@ conn.query("DROP TABLE IF EXISTS ans")
 question = "median v3 sd v3 by id4 id5" # q6
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id4, id5, medianExact(v3) AS median_v3, stddevPop(v3) AS sd_v3 FROM db_benchmark.x GROUP BY id4, id5"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id4, id5, medianExact(v3) AS median_v3, stddevPop(v3) AS sd_v3 FROM db_benchmark.x GROUP BY id4, id5 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -233,7 +237,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id4, id5, medianExact(v3) AS median_v3, stddevPop(v3) AS sd_v3 FROM db_benchmark.x GROUP BY id4, id5"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id4, id5, medianExact(v3) AS median_v3, stddevPop(v3) AS sd_v3 FROM db_benchmark.x GROUP BY id4, id5 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -251,7 +255,7 @@ conn.query("DROP TABLE IF EXISTS ans")
 question = "max v1 - min v2 by id3" # q7
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id3, max(v1) - min(v2) AS range_v1_v2 FROM db_benchmark.x GROUP BY id3"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id3, max(v1) - min(v2) AS range_v1_v2 FROM db_benchmark.x GROUP BY id3 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -265,7 +269,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id3, max(v1) - min(v2) AS range_v1_v2 FROM db_benchmark.x GROUP BY id3"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id3, max(v1) - min(v2) AS range_v1_v2 FROM db_benchmark.x GROUP BY id3 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -283,7 +287,7 @@ conn.query("DROP TABLE IF EXISTS ans")
 question = "largest two v3 by id6" # q8
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id6, arrayJoin(arraySlice(arrayReverseSort(groupArray(v3)), 1, 2)) AS largest2_v3 FROM (SELECT id6, v3 FROM db_benchmark.x WHERE v3 IS NOT NULL) AS subq GROUP BY id6"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id6, arrayJoin(arraySlice(arrayReverseSort(groupArray(v3)), 1, 2)) AS largest2_v3 FROM (SELECT id6, v3 FROM db_benchmark.x WHERE v3 IS NOT NULL) AS subq GROUP BY id6 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -297,7 +301,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id6, arrayJoin(arraySlice(arrayReverseSort(groupArray(v3)), 1, 2)) AS largest2_v3 FROM (SELECT id6, v3 FROM db_benchmark.x WHERE v3 IS NOT NULL) AS subq GROUP BY id6"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id6, arrayJoin(arraySlice(arrayReverseSort(groupArray(v3)), 1, 2)) AS largest2_v3 FROM (SELECT id6, v3 FROM db_benchmark.x WHERE v3 IS NOT NULL) AS subq GROUP BY id6 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -315,7 +319,7 @@ conn.query("DROP TABLE IF EXISTS ans")
 question = "regression v1 v2 by id2 id4" # q9
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id2, id4, pow(corr(v1, v2), 2) AS r2 FROM db_benchmark.x GROUP BY id2, id4"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id2, id4, pow(corr(v1, v2), 2) AS r2 FROM db_benchmark.x GROUP BY id2, id4 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -329,7 +333,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id2, id4, pow(corr(v1, v2), 2) AS r2 FROM db_benchmark.x GROUP BY id2, id4"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id2, id4, pow(corr(v1, v2), 2) AS r2 FROM db_benchmark.x GROUP BY id2, id4 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -347,7 +351,7 @@ conn.query("DROP TABLE IF EXISTS ans")
 question = "sum v3 count by id1:id6" # q10
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, id2, id3, id4, id5, id6, sum(v3) AS v3, count() AS cnt FROM db_benchmark.x GROUP BY id1, id2, id3, id4, id5, id6"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, id2, id3, id4, id5, id6, sum(v3) AS v3, count() AS cnt FROM db_benchmark.x GROUP BY id1, id2, id3, id4, id5, id6 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
@@ -361,7 +365,7 @@ write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_row
 conn.query("DROP TABLE IF EXISTS ans")
 gc.collect()
 t_start = timeit.default_timer()
-QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, id2, id3, id4, id5, id6, sum(v3) AS v3, count() AS cnt FROM db_benchmark.x GROUP BY id1, id2, id3, id4, id5, id6"
+QUERY=f"CREATE TABLE ans {query_engine} AS SELECT id1, id2, id3, id4, id5, id6, sum(v3) AS v3, count() AS cnt FROM db_benchmark.x GROUP BY id1, id2, id3, id4, id5, id6 {settings}"
 conn.query(QUERY)
 nr=str(conn.query("SELECT count(*) AS cnt FROM ans"))
 nc=str(conn.query("SELECT * FROM ans LIMIT 0"))
